85fa290293ddf276a5c2993e40da71a6f31b42b5
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2015-12-24 09:09:11 +0000

    [OI] - cleaned up extraneous files

--- jobs/docker/docker-container-run.sh
+++
@@ -1,8 +0,0 @@
-export PYTHONUNBUFFERED=1
-export ANSIBLE_FORCE_COLOR=true
-
-EXTRA_VARS="bukt_app_name=${APP_NAME} bukt_app_version=${APP_VERSION} dockerize_task=container_run dockerize_host_bind_port=$APP_PORT bukt_app_port=$APP_PORT" 
- 
-cd $WORKSPACE/ansible/
-RAX_CREDS_FILE=~/.rax-creds ansible-playbook -i inventory/rax.py products/ci/docker_ci.yml -e "$EXTRA_VARS" \
-            -u ubuntu --private-key ~/.ssh/ubuntu --vault-password-file ~/.bukt-vault.pass 
--- jobs/serviceJobs.groovy
+++ jobs/serviceJobs.groovy
@@ -1,101 +1,24 @@
-services = [
-    "accounts-service"      : "3030",
-    "agent-portal"          : "7070",
-    "collections-service"   : "9090",
-    "consumer-portal"       : "2020",
-    "notifications-service" : "5050",
-    "timeline-service"      : "6060"
-    ]
-
-docker_jobs = [ 
-    "image-build": "docker-image-build", 
-    "container-run": "docker-container-run" 
-    ]
-
-services.each { svc, svcPort ->
-    def dockerWrapperJob = "${svc}-dockerize"
-
-    /*
-        service/component build job
-    */
-    println "svc-job = ${svc}"
-    job(svc) {
-        scm {
-            git {
-                remote {
-                    github('wunzeco/ee-microdc-helloworld')
-                }
-                branch('master')
+/*
+    service/component build job
+*/
+svc = 'helloworld'
+job(svc) {
+    scm {
+        git {
+            remote {
+                github("wunzeco/ee-microdc-${svc}")
             }
-        }
-        triggers {
-            scm('H/2 * * * *')
-        }
-        steps {
-            environmentVariables {
-                envs(APP_NAME: svc, APP_VERSION: '${BUILD_NUMBER}')
-            }
-            shell("./gradlew clean build createDockerfile distTarGz")
-            downstreamParameterized {
-                trigger(dockerWrapperJob) {
-                    block {
-                        buildStepFailure('FAILURE')
-                        failure('FAILURE')
-                        unstable('UNSTABLE')
-                    }
-                    parameters {
-                        predefinedProp('APP_NAME', svc)
-                        predefinedProp('APP_VERSION', '$BUILD_NUMBER')
-                        predefinedProp('APP_PORT', svcPort)
-                    }
-                }
-            }
-        }
-        wrappers {
-            credentialsBinding {
-                usernamePassword('NEXUS_LOGIN', 'bukt-nexus-login-creds-id')
-            }
-        }
-        publishers {
-            archiveJunit '**/test-results/*.xml'
+            branch('master')
         }
     }
-
-    
-    /*
-        svc-dockerize - a wrapper job that triggers docker image & container jobs
-    */
-    println "svc-dockerize = ${dockerWrapperJob}"
-    job(dockerWrapperJob) {
-        parameters {
-            stringParam('APP_NAME',     defaultValue = '', description = 'application name')
-            stringParam('APP_VERSION',  defaultValue = '', description = 'application version')
-            stringParam('APP_PORT',     defaultValue = '', description = 'application port')
-        }
-        steps {
-            downstreamParameterized {
-                trigger(docker_jobs["image-build"]) {
-                    block {
-                        buildStepFailure('FAILURE')
-                        failure('FAILURE')
-                        unstable('UNSTABLE')
-                    }
-                    parameters {
-                        currentBuild()
-                    }
-                }
-                trigger(docker_jobs["container-run"]) {
-                    block {
-                        buildStepFailure('FAILURE')
-                        failure('FAILURE')
-                        unstable('UNSTABLE')
-                    }
-                    parameters {
-                        currentBuild()
-                    }
-                }
-            }
-        }
+    triggers {
+        scm('H/2 * * * *')
     }
-
+    steps {
+        shell("./gradlew clean build createDockerfile distTarGz")
+    }
+    publishers {
+        archiveJunit '**/test-results/*.xml'
+    }
 }
+
