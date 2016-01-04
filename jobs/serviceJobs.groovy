3a06502237c3532d608784273935a5bdc4ad46e6
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-01-04 13:54:10 +0000

    [OI] publish artifacts

--- jobs/serviceJobs.groovy
+++ jobs/serviceJobs.groovy
@@ -16,9 +16,36 @@ job(svc) {
     }
     steps {
         shell("./gradlew clean build createDockerfile distTarGz")
+        shell("Now upload application artifact (${svc}-${BUILD_NUMBER}.tar.gz) to Nexus/S3")
     }
     publishers {
         archiveJunit '**/test-results/*.xml'
+        archiveArtifacts {
+            pattern('**/build/lib/"${svc}*.jar')
+            pattern('**/build/docker/Dockerfile')
+            onlyIfSuccessful()
+        }
     }
 }
 
+/*
+job("${svc}-image-build") {
+    steps {
+        //copy artifact from $svc
+        copyArtifacts(svc) {
+            includePatterns('*.jar', 'Dockerfile')
+            //excludePatterns('test.xml', 'test.properties')
+            targetDirectory('files')
+            flatten()
+            optional()
+            buildSelector {
+                latestSuccessful(true)
+            }
+        }
+        //decompress artifact
+        // build image
+        // I need Dockerfile, jar and docker-compose.yml
+        //run container
+    }
+}
+*/
